08b3a4b44b7180f9376673fccd673e536ad35cab
Harsh Gautam <hrshgtm9@gmail.com>
2020-04-27 20:52:13 +0530

    fixed listoflinks width problem

--- app/components/views/ListOfLinksView.tsx
+++ app/components/views/ListOfLinksView.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
 import React from 'react';
+import Truncate from 'react-truncate';
 import styles from '../Panel.scss';
 import { copyAndExit } from '../../utils/clipboard';
 import { ResponseTypes, LinkItemType } from '../../bots/types';
@@ -13,36 +14,46 @@ type Props = {
 
 export default function ListOfLinks(props: Props) {
   const { data } = props;
+
+  const dots = <span>...</span>;
+  const truncatedText = (text, width) => (
+    <Truncate width={width} ellipsis={dots}>
+      {text}
+    </Truncate>
+  );
+
   const links = data.map((linkItem: LinkItemType, index: number) => {
     return (
-      <div key={index} className={`column box ${styles.linkContainer}`}>
-        <div className="level is-mobile">
-          <div className="level-left">
-            <div>
+      <div key={index} className={`box ${styles.linkContainer}`}>
+        <div className="columns">
+          <div className="column">
+            <div className="is-pulled-left">
               <h3 className="is-size-3 has-text-weight-light">
-                {linkItem.title}
+                {truncatedText(linkItem.title, 370)}
               </h3>
-              <span className="is-size-6 has-text-weight-light has-text-grey">
-                {linkItem.link}
+              <span className="is-size-7 has-text-weight-light has-text-grey">
+                {truncatedText(linkItem.link, 340)}
               </span>
             </div>
           </div>
-          <div className="level-right">
-            <div className="buttons has-addons">
-              <button
-                type="button"
-                onClick={() => copyAndExit(linkItem.link, ResponseTypes.Text)}
-                className={`button ${styles.copyBtn} `}
-              >
-                Copy Link
-              </button>
-              <button
-                type="button"
-                onClick={e => openLinkInDefaultBrowser(e, linkItem.link)}
-                className={`button ${styles.copyBtn} `}
-              >
-                Open Link
-              </button>
+          <div className="column">
+            <div className="is-pulled-right">
+              <div className="buttons has-addons">
+                <button
+                  type="button"
+                  onClick={() => copyAndExit(linkItem.link, ResponseTypes.Text)}
+                  className={`button is-small ${styles.copyBtn} `}
+                >
+                  Copy Link
+                </button>
+                <button
+                  type="button"
+                  onClick={e => openLinkInDefaultBrowser(e, linkItem.link)}
+                  className={`button is-small ${styles.copyBtn} `}
+                >
+                  Open Link
+                </button>
+              </div>
             </div>
           </div>
         </div>
