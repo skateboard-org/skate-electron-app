e334476d6c1e24152f87a8c7ce3b504d04a5ba5a
Harsh Gautam <hrshgtm9@gmail.com>
2020-04-27 20:52:35 +0530

    send null as param, if param is not present

--- app/bots/CloudBots.ts
+++ app/bots/CloudBots.ts
@@ -3,8 +3,12 @@ import axios from 'axios';
 const apiUrl = 'https://us-central1-skate-board.cloudfunctions.net/default/api';
 
 const executeCloudBot = async (botName: string, botParam: string) => {
+  let param = botParam;
+  if (botParam === undefined || botParam.length === 0) {
+    param = 'null';
+  }
   const data = await axios
-    .get(`${apiUrl}/bot/exec/${botName}/${botParam}`)
+    .get(`${apiUrl}/bot/exec/${botName}/${param}`)
     .then((res: any) => {
       return res.data;
     })
