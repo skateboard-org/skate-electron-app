b7b825e7d96d8f80ab29ef29291c1fd75b36a5ac
Harsh Gautam <hrshgtm9@gmail.com>
2020-05-02 05:29:12 +0530

    added animtion to light

--- app/components/Board/StatusLight/index.tsx
+++ app/components/Board/StatusLight/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimateKeyframes } from 'react-simple-animate';
 import {
   CommandStatusType,
   processCommandStatus
@@ -31,36 +32,36 @@ export default function Loader(props: Props) {
     );
   };
 
-  const onMouseEnterStatusLight = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    message: string | null
-  ) => {
-    console.log('hello', message);
-  };
-
-  const onMouseOutStatusLight = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    message: string | null
-  ) => {
-    console.log('hello', message);
-  };
-
   const statusLight = (isVisible: boolean, status: CommandStatusType) => {
     if (isVisible) {
       const { colour, message, animate } = processCommandStatus(status);
 
-      const animationClass = animate ? 'animation-wiggle' : '';
+      console.log(colour, animate);
 
       const tooltipClass = 'has-tooltip-left has-tooltip-active';
-
       return (
-        // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
         <div
-          onMouseEnter={e => onMouseEnterStatusLight(e, message)}
-          onMouseOut={e => onMouseOutStatusLight(e, message)}
           data-tooltip={message}
-          className={`status-light ${colour} ${tooltipClass} ${animationClass}`}
-        />
+          className={`status-light-container ${tooltipClass}`}
+        >
+          <AnimateKeyframes
+            play={animate} // Toggle when animation should start
+            duration={0.5}
+            keyframes={[
+              'animation: rubberband cubic-bezier(.36,.07,.19,.97) both',
+              'backface-visibility: hidden',
+              { 0: 'transform: scale3d(1, 1, 1)' },
+              { 30: 'transform: scale3d(1.25, 0.75, 1)' },
+              { 40: 'transform: scale3d(0.75, 1.25, 1)' },
+              { 50: 'transform: scale3d(1.15, 0.85, 1)' },
+              { 65: 'transform: scale3d(0.95, 1.05, 1)' },
+              { 75: 'transform: scale3d(1.05, 0.95, 1)' },
+              { 100: 'transform: scale3d(1, 1, 1)' }
+            ]}
+          >
+            <div className={`status-light ${colour}`} />
+          </AnimateKeyframes>
+        </div>
       );
     }
     return null;
