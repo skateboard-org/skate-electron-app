f08b3a511a98eef890e2bb4d42f03c96a60a9f40
Harsh Gautam <hrshgtm9@gmail.com>
2020-05-04 21:18:07 +0530

    removed animation

--- app/components/Board/StatusLight/index.tsx
+++ app/components/Board/StatusLight/index.tsx
@@ -34,9 +34,7 @@ export default function Loader(props: Props) {
 
   const statusLight = (isVisible: boolean, status: CommandStatusType) => {
     if (isVisible) {
-      const { colour, message, animate } = processCommandStatus(status);
-
-      console.log(colour, animate);
+      const { colour, message } = processCommandStatus(status);
 
       const tooltipClass = 'has-tooltip-left has-tooltip-active';
       return (
@@ -44,23 +42,7 @@ export default function Loader(props: Props) {
           data-tooltip={message}
           className={`status-light-container ${tooltipClass}`}
         >
-          <AnimateKeyframes
-            play={animate} // Toggle when animation should start
-            duration={0.5}
-            keyframes={[
-              'animation: rubberband cubic-bezier(.36,.07,.19,.97) both',
-              'backface-visibility: hidden',
-              { 0: 'transform: scale3d(1, 1, 1)' },
-              { 30: 'transform: scale3d(1.25, 0.75, 1)' },
-              { 40: 'transform: scale3d(0.75, 1.25, 1)' },
-              { 50: 'transform: scale3d(1.15, 0.85, 1)' },
-              { 65: 'transform: scale3d(0.95, 1.05, 1)' },
-              { 75: 'transform: scale3d(1.05, 0.95, 1)' },
-              { 100: 'transform: scale3d(1, 1, 1)' }
-            ]}
-          >
-            <div className={`status-light ${colour}`} />
-          </AnimateKeyframes>
+          <div className={`status-light ${colour}`} />
         </div>
       );
     }
