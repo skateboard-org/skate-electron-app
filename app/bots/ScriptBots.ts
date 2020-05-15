023144a0e8ed67cbd817bed58a4dbac7b04d5955
Harsh Gautam <hrshgtm9@gmail.com>
2020-05-16 01:10:10 +0530

    added botCache folder, added botSource param

--- app/bots/ScriptBots.ts
+++ app/bots/ScriptBots.ts
@@ -1,4 +1,8 @@
-const executeScriptBot = async (botName: string, botParam: string) => {
+const executeScriptBot = async (
+  botName: string,
+  botParam: string,
+  botSource: string
+) => {
   let param = botParam;
   if (botParam === undefined || botParam.length === 0) {
     param = 'null';
