41c5059d6063fae53def29c9029e1f2ee77beb47
Harsh Gautam <hrshgtm9@gmail.com>
2020-05-15 22:55:23 +0530

    script to load env vars

---
+++ load-env-vars.sh
@@ -0,0 +1,6 @@
+# export CSC_LINK="/Volumes/Files/Projects/skate/skate-electron-app/code-sign/Certificates.p12"
+# export CSC_KEY_PASSWORD="code-sign-password"
+# export CSC_NAME="Apple Development: hrshgtm5@gmail.com (RKQU74X96M)"
+export CSC_IDENTITY_AUTO_DISCOVERY=false
+export DEBUG=electron-builder
+
